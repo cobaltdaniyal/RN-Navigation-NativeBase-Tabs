13855d40470aa61a85baa4c76fe316395f69e57b
Daniyal Kaleem <75447252+cobaltdaniyal@users.noreply.github.com>
2021-02-18 22:22:22 +0500

    second commit

--- src/screens/Tabone.js
+++ src/screens/Tabone.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
-import { View, Text } from 'react-native';
+import { Container, Header, Content, Form, Item, Input, Label, Button, Text } from 'native-base';
+import { Picker } from '@react-native-picker/picker';
 
 
 class TabOne extends Component {
+    state = {
+        language: 'java',
+    };
     render() {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-                <Text>Tab One</Text>
-            </View>
+            <Container>
+                <Content>
+                    <Form>
+                        <Item style={{ padding: 5 }} floatingLabel>
+                            <Label>Username</Label>
+                            <Input />
+                        </Item>
+                        <Item style={{ padding: 5 }} floatingLabel last>
+                            <Label>Password</Label>
+                            <Input />
+                        </Item>
+
+                        {/* Picker Start >>>>>>>>>>>>>>>>>>>> */}
+                        <Picker
+                            selectedValue={this.state.language}
+                            style={{ height: 50, width: 150 }}
+                            onValueChange={(itemValue, itemIndex) =>
+                                this.setState({ language: itemValue })
+                            }>
+                            <Picker.Item label="Java" value="java" />
+                            <Picker.Item label="JavaScript" value="js" />
+                        </Picker>
+                        {/* Picker Enf >>>>>>>>>>>>>>>>>>>> */}
+
+                    </Form>
+                    <Button style={{ margin: 10 }} full success>
+                        <Text>Success</Text>
+                    </Button>
+                </Content>
+            </Container>
         )
     }
 }
